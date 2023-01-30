73bb598cb9acd518b41a1f3a6d0cfca0b4ed3731
Kade <kadekorn@gmail.com>
2023-01-30 07:15:02 -0700

    this problem took me all hour to do, but I feel pretty good about it now which is great.clear The next question looks doable. Looking forward to tomorrow morning

---
+++ app.js
@@ -0,0 +1,50 @@
+//Q1                 // Math.floor rounds down the number so decimals dont add a full number
+// function showRatings(rating){
+//     let ratings = "";
+//     for (let i = 0; i < Math.floor(rating); i++){ // or let i = 1 and i <= rating, so then you can get rid of -1 on line six
+//         ratings = ratings + "*";
+//         if (i !== Math.floor(ratings) - 1){ // <-- says if not last rating "*" give it a space
+//             ratings = ratings + " "
+//         }
+//     }
+//     if (!Number.isInteger(rating)){ // original (rating = .5)
+//         ratings = ratings + "."
+//     }
+//     return ratings;
+// }
+
+//^ original. Below is cleaned up with plus equals
+// function showRatings(rating) {
+//   let ratings = "";
+//   for (let i = 0; i < Math.floor(rating); i++) {
+//     // or let i = 1 and i <= rating, so then you can get rid of -1 on line six
+//     ratings += "*";
+//     if (i !== Math.floor(ratings) - 1) {
+//       // <-- says if not last rating "*" give it a space
+//       ratings += " ";
+//     }
+//   }
+//   if (!Number.isInteger(rating)) {
+//     // original (rating = .5)
+//     ratings += ".";
+//   }
+//   return ratings;
+// }
+
+// Doing Q1 again,
+
+function showRatings(rating) {
+  let ratings = "";
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratings += "*";
+    if (i !== Math.floor(ratings) - 1) {
+      ratings += " ";
+    }
+  }
+  if (!Number.isInteger(rating)) {
+    ratings += ".";
+  }
+  return ratings;
+}
+
+console.log(showRatings(4.5));
